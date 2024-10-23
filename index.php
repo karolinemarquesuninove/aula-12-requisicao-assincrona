5bfae637a168d8ea233ca8fe6f4b02eaa4b42206
karolinemarquesuninove <131720140+karolinemarquesuninove@users.noreply.github.com>
2024-10-23 01:00:30 +0000

    criar projeto

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h1>Calculadora AJAX</h1>
+
+     <pre>
+     <label> Numero 01</label>
+     <input type= "text" name="numero1" id="numero1"/>
+
+     <label>Numero 02
+     <input type= "text" name="numero1" id="numero1"/>
+
+     <button onclick = "calcular();">Calcular com ajax</button>
+     <p id="resultado"></p>
+</pre>
+<script>
+    function calcular(){
+        const numero1 = document.getElementById("numero1"). value;
+        const numero2 = document.getElementById("numero2"). value;
+
+        fetch('/calculo.php', {
+            method: 'POST' ,
+            headers: {'Content-Type': 'applications/json'},
+            body: JSON.stringify({
+                numero1: parseFloat(numero1),
+                numero2: parseFloat(numero2)
+
+            })
+        })
+        .then(resposta=>resposta.json())
+        .then(dados=>{
+        document.getElementById("resultado").innerHTML=
+        "Soma: " + dados.soma;
+
+    })
+    .catch(erro=>){
+        document.getElementById("resultado").innerHTML=
+        "Erro ao processar";
+    });
+
+></body>
+</html>
